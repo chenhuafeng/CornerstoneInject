55ce8e08330dcd02821cea21bdec2c79c7e7a62b
陈华锋 <chenhuafeng@yy.com>
2019-01-07 21:03:01 +0800

    破解成功

---
+++ CornerstoneInject/CornerstoneInject.h
@@ -0,0 +1,13 @@
+//
+//  CornerstoneInject.h
+//  CornerstoneInject
+//
+//  Created by huafeng chen on 2019/1/7.
+//  Copyright © 2019 huafeng chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (CornerstoneInject)
+
+@end
--- CornerstoneInject/CornerstoneInject.m
+++ CornerstoneInject/CornerstoneInject.m
@@ -6,13 +6,9 @@
 //  Copyright © 2019 huafeng chen. All rights reserved.
 //
 
-#import "fishhook.h"
+#import "CornerstoneInject.h"
 #import "JRSwizzle.h"
 #import "ZSubscriptionLicensingPolicy.h"
-
-@interface NSObject (CornerstoneInject)
-
-@end
 
 @implementation NSObject (CornerstoneInject)
 
@@ -23,21 +19,11 @@
 }
 
 - (BOOL)hf_apply {
-    [self hf_apply];
     return YES;
 }
 
 - (NSData *)hf_licensingKey {
-    return [self hf_licensingKey] ?: [NSData data];
+    return [NSData data];
 }
 
 @end
-
-static int (*orig_sysctlbyname)(const char *, void *, size_t *, void *, size_t) = NULL;
-int hf_sysctlbyname(const char *name, void *b, size_t *c, void *d, size_t e) {
-    return 0;
-}
-
-__attribute__((constructor)) static void entry() {
-    rebind_symbols((struct rebinding[1]){{"sysctlbyname", hf_sysctlbyname, (void *)&orig_sysctlbyname}},1);
-}
